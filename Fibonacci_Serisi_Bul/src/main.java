cc7647c632d5515d03507fa03d48fdfc4cb94305
mehmetdoganay <mehmetdganay@gmail.com>
2023-06-28 14:16:39 +0300

    Fibonacci serisi bulan uygulama.

---
+++ Fibonacci_Serisi_Bul/src/main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Seri Kaç elemanlı olsun? ");
+        int fibonacci = 0, num1 = 1, num2 = 0, seriSayisi = input.nextInt();
+
+        for (int i = 0; i < seriSayisi; i++) {
+            System.out.println(num2 + " + " + num1 + " = " + fibonacci);
+            fibonacci = num2 + num1;
+            num2= num1;
+            num1 = fibonacci;
+        }
+    }
+}
