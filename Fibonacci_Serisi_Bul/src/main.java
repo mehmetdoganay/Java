ec353031b0b389edcb688a3727332335ffb59233
mehmetdoganay <mehmetdganay@gmail.com>
2023-07-01 00:37:12 +0300

    Fibonacci dizisinde sıralama düzeltildi

--- Fibonacci_Serisi_Bul/src/main.java
+++ Fibonacci_Serisi_Bul/src/main.java
@@ -7,8 +7,8 @@ public class main {
         int fibonacci = 0, num1 = 1, num2 = 0, seriSayisi = input.nextInt();
 
         for (int i = 0; i < seriSayisi; i++) {
-            System.out.println(num2 + " + " + num1 + " = " + fibonacci);
             fibonacci = num2 + num1;
+            System.out.println(num2 + " + " + num1 + " = " + fibonacci);
             num2= num1;
             num1 = fibonacci;
         }
