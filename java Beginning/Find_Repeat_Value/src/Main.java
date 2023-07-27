666613f36dd5b8723db3742a47c59726b754ed4f
mehmetdoganay <mehmetdganay@gmail.com>
2023-07-27 15:25:46 +0300

    find repeat value num

--- java Beginning/Find_Repeat_Value/src/Main.java
+++ java Beginning/Find_Repeat_Value/src/Main.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Main {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void findDuplicateElements(int[] arr)
+    {
+        HashMap<Integer,Integer> frequencyMap = new HashMap<>();
+
+        for(int num : arr)
+        {
+            if(frequencyMap.containsKey(num)) // num anahtarı var mı ?
+            {
+                int currentFrequency = frequencyMap.get(num); // num değerine ait frekansı atadık
+                frequencyMap.put(num,currentFrequency + 1); // num değerine ait frekansi 1 artırdık.
+            }
+            else {
+                frequencyMap.put(num, 1); // yoksa değer 1 kalsın
+            }
+        }
+        for(Map.Entry<Integer,Integer> entry : frequencyMap.entrySet())
+        {
+            if(entry.getValue() > 1)
+            {
+                System.out.println(frequencyMap.containsKey() + " Sayısı " + entry.getValue() + "kez tekrar edmiştir.");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] list = {10, 20, 20, 10, 10, 20, 5, 20};
+        findDuplicateElements(list);
+    }
 }
